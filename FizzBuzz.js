971f7da5482977d5beacce8f573e4ade41a6b457
Galaxeo <hwjustincheok@gmail.com>
2023-02-09 12:33:08 -0500

    chore: practice js with FizzBuzz problem

---
+++ FizzBuzz.js
@@ -0,0 +1,15 @@
+function FizzBuzz(num){
+    let str = "";
+    if (num%3==0){str+="Fizz";}
+    if (num%5==0){str+="Buzz";}
+    return str;
+}
+let answer = 15;//parseInt(prompt("Please enter number for FizzBuzz"))
+for (let i = 1; i<=answer; i++){
+    if (FizzBuzz(i) == ""){
+        console.log(i);
+    }
+    else{
+        console.log(FizzBuzz(i));
+    }
+}
