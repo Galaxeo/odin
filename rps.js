ba5fe183ddeaeb45226d76ec6dcf413ec13c57b6
Galaxeo <hwjustincheok@gmail.com>
2023-02-13 09:18:15 -0500

    feat: make rps.js functional

--- rps.js
+++ rps.js
@@ -1,13 +1,49 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)+1;
-    if (choice == 1){
-        return "rock";
-    }
-    else if (choice == 2){
-        return "paper";
-    }
-    else if (choice == 3){
-        return "scissors";
-    }
+    /*1 = rock, 2 = paper, 3 = scissors*/
+    return choice;
 }
 console.log(getComputerChoice());
+function rps(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    let choice = 0;
+    if (playerSelection == 'rock'){
+        if (computerSelection == 1){
+            return "Tie"
+        }
+        else if (computerSelection == 2){
+            return "You lost"
+        }
+        else if (computerSelection == 3){
+            return "You won" 
+        }
+    } else if (playerSelection == 'paper'){
+        if (computerSelection == 2){
+            return "Tie" 
+        }
+        else if (computerSelection == 3){
+            return "You lost" 
+        }
+        else if (computerSelection == 1){
+            return "You won" 
+        }
+    } else if (playerSelection == 'scissors'){
+        if (computerSelection == 3){
+            return "Tie" 
+        }
+        else if (computerSelection == 1){
+            return "You lost" 
+        }
+        else if (computerSelection == 2){
+            return "You won" 
+        }
+    } else {
+        return "Here"
+    }
+}
+function game(){
+    for (let i = 0; i < 5; i++){
+        console.log(rps(prompt('enter prompt \n'), getComputerChoice()))
+    }
+}
+game()
