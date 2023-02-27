21520141bceabdedf3f7e5b081da7a67d7918a72
Galaxeo <hwjustincheok@gmail.com>
2023-02-27 18:53:17 -0500

    chore: lazygit demo

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,16 @@
-function FizzBuzz(num){
-    let str = "";
-    if (num%3==0){str+="Fizz";}
-    if (num%5==0){str+="Buzz";}
-    return str;
+function FizzBuzz(num) {
+  let str = "";
+  if (num % 3 == 0) { str += "Fizz"; }
+  if (num % 5 == 0) { str += "Buzz"; }
+  return str;
 }
+let unused = 12;
 let answer = 15;//parseInt(prompt("Please enter number for FizzBuzz"))
-for (let i = 1; i<=answer; i++){
-    if (FizzBuzz(i) == ""){
-        console.log(i);
-    }
-    else{
-        console.log(FizzBuzz(i));
-    }
+for (let i = 1; i <= answer; i++) {
+  if (FizzBuzz(i) == "") {
+    console.log(i);
+  }
+  else {
+    console.log(FizzBuzz(i));
+  }
 }
