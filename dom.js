5824542a2ecb27283de849042bf18ac6e66e02ab
Galaxeo <hwjustincheok@gmail.com>
2023-04-07 17:13:14 -0400

    feat: add button to dom.html and fix original problem

--- dom.js
+++ dom.js
@@ -4,6 +4,10 @@ redp.style.cssText = 'color: red;';
 redp.textContent = "Hey I'm red!";
 h3.style.cssText = 'color: blue;';
 h3.textContent = "I'm a blue h3!";
+
+document.body.appendChild(redp);
+document.body.appendChild(h3);
+
 const div = document.createElement('div');
 const meToo = document.createElement('p');
 const h1 = document.createElement('h1');
@@ -12,4 +16,10 @@ div.appendChild(h1);
 div.appendChild(meToo);
 h1.textContent = "I'm in a div";
 meToo.textContent = "ME TOO!";
+document.body.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
 
