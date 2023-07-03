a1a488775758b041a9d5be82302acdd21ccad242
Galaxeo <hwjustincheok@gmail.com>
2023-07-03 16:21:39 -0400

    feat: add library display and form toggle

--- library/script.js
+++ library/script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+let docLib = document.querySelector('.library');
+
 function Book(title, author, numPages, readStatus) {
   this.title = title;
   this.author = author;
@@ -8,25 +10,28 @@ function Book(title, author, numPages, readStatus) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  var addBook = document.createElement('div');
+  addBook.classList.add('book');
+  // Add Book
+  var bookTitle = document.createElement('h1');
+  bookTitle.classList.add("bookTitle");
+  bookTitle.innerHTML = book.title;
+  addBook.appendChild(bookTitle);
+  var bookAuthor = document.createElement('h2');
+  bookAuthor.classList.add("bookAuthor");
+  bookAuthor.innerHTML = book.author;
+  addBook.appendChild(bookAuthor);
+  var bookPages = document.createElement('p');
+  bookPages.classList.add("numPages");
+  bookPages.innerHTML = book.numPages;
+  addBook.appendChild(bookPages);
+  var bookRead = document.createElement('p');
+  bookRead.classList.add("readStatus");
+  bookRead.innerHTML = book.readStatus;
+  addBook.appendChild(bookRead);
+
+  docLib.appendChild(addBook);
 }
-
-function printDog() {
-
-  console.log("here");
-}
-
-const btn = document.querySelector(".submit");
-const newBookBtn = document.querySelector(".newBook");
-
-btn.addEventListener("click", (event) => {
-  event.preventDefault();
-  newBook();
-})
-
-newBookBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  document.querySelector(".bookForm").style.display = "block";
-})
 
 function newBook() {
   var title = document.getElementById('title').value;
@@ -38,3 +43,17 @@ function newBook() {
   console.log(book.title, book.author, book.numPages, book.readStatus);
   return book;
 }
+
+const btn = document.querySelector(".submit");
+const newBookBtn = document.querySelector(".newBook");
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary(newBook());
+  document.querySelector("#formContainer").classList.toggle("hidden");
+})
+
+newBookBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  document.querySelector("#formContainer").classList.toggle("hidden");
+})
