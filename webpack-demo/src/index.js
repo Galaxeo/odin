f37264c1e285fb935b6127cce19bc131cf97943e
Galaxeo <hwjustincheok@gmail.com>
2023-07-12 17:12:27 -0400

    feat: webpack loading font

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
-import myName from './myName';
+import './style.css';
+import Img from './image.png';
 
 function component() {
   const element = document.createElement('div');
 
-  element.textContent = myName('Justin');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myImg = new Image();
+  myImg.src = Img;
+
+  element.appendChild(myImg);
+
   return element;
 }
 
