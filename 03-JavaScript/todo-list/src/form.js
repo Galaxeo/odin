5804ba2636ef0e52634a49155b32dbacd4f8b3e2
Galaxeo <hwjustincheok@gmail.com>
2023-07-18 22:24:00 -0400

    feat: add fun effect on click header, start form

---
+++ 03-JavaScript/todo-list/src/form.js
@@ -0,0 +1,16 @@
+import formHelper from './formHelper.js';
+function createForm() {
+  const formCont = document.createElement('div');
+  formCont.classList.add('formCont');
+
+  const form = document.createElement('form');
+  form.classList.add('cardForm');
+
+  const taskRow = formHelper('Task');
+
+  form.appendChild(taskRow);
+
+  formCont.appendChild(form);
+  return formCont;
+}
+export default createForm;
