c3e3ac63cea885e324bb806b66ba64ceb0b81de1
Galaxeo <hwjustincheok@gmail.com>
2023-09-13 16:21:41 -0400

    chore: continue recursive review

--- 03-JavaScript/computer-science/recursion.js
+++ 03-JavaScript/computer-science/recursion.js
@@ -16,6 +16,32 @@ function factorial(num) {
   }
   return num * factorial(num - 1);
 }
-console.log(sumRange(3));
-console.log(power(2, 4));
-console.log(factorial(5));
+function less(num) {
+  return num < 7;
+}
+
+function all2(arr, callback) {
+  const slice = arr.pop();
+  if (arr.length == 0) {
+    return true;
+  }
+  if (callback(slice)) {
+    return all2(arr, callback);
+  } else {
+    return false;
+  }
+}
+
+function prodOfArr(arr) {
+  const dog = arr.pop();
+  if (arr.length == 0) {
+    return dog;
+  }
+  return dog * prodOfArr(arr);
+}
+
+// console.log(sumRange(3));
+// console.log(power(2, 4));
+// console.log(factorial(5));
+// console.log(all2([1, 9, 3], less))
+console.log(prodOfArr([1, 2, 8]))
