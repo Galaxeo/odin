5737e3f14c5cc6d7a8d7c6d5e036ede4b84a841b
Galaxeo <hwjustincheok@gmail.com>
2023-09-16 12:21:47 -0400

    feat: finish merge sort assignment

--- 03-JavaScript/computer-science/Session.vim
+++ 03-JavaScript/computer-science/Session.vim
@@ -13,17 +13,17 @@ if &shortmess =~ 'A'
 else
   set shortmess=aoO
 endif
-badd +25 ~/code/odin/03-JavaScript/computer-science/fibonacci.js
+badd +40 ~/code/odin/03-JavaScript/computer-science/mergesort.js
 argglobal
 %argdel
-edit ~/code/odin/03-JavaScript/computer-science/fibonacci.js
+edit ~/code/odin/03-JavaScript/computer-science/mergesort.js
 argglobal
-let s:l = 27 - ((26 * winheight(0) + 20) / 40)
+let s:l = 5 - ((4 * winheight(0) + 20) / 41)
 if s:l < 1 | let s:l = 1 | endif
 keepjumps exe s:l
 normal! zt
-keepjumps 27
-normal! 0
+keepjumps 5
+normal! 019|
 tabnext 1
 if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
   silent exe 'bwipe ' . s:wipebuf
---
+++ 03-JavaScript/computer-science/mergesort.js
@@ -0,0 +1,38 @@
+function mergeSort(lst) {
+  let mid = Math.floor(lst.length / 2);
+  let left = lst.slice(0, mid);
+  let right = lst.slice(mid);
+  if (left.length > 1) {
+    mergeSort(left);
+  }
+  if (right.length > 1) {
+    mergeSort(right);
+  }
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      lst[k] = left[i];
+      k += 1;
+      i += 1;
+    } else {
+      lst[k] = right[j];
+      k += 1;
+      j += 1;
+    }
+  }
+  while (j < right.length) {
+    lst[k] = right[j];
+    k += 1;
+    j += 1;
+  }
+  while (i < left.length) {
+    lst[k] = left[i];
+    k += 1;
+    i += 1;
+  }
+}
+lst1 = [6, 1, 3, 7, 2, 5, 4];
+mergeSort(lst1);
+console.log(lst1);
