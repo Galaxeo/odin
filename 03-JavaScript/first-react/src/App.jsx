64a1080a005b7afe93c9d4b1f232bf68f24defc3
Galaxeo <hwjustincheok@gmail.com>
2023-10-30 14:06:07 -0400

    feat: animals example

--- 03-JavaScript/first-react/src/App.jsx
+++ 03-JavaScript/first-react/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+function ListItem(props) {
+  return <li>{props.animal}</li>
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return animal.startsWith("L") ? <ListItem key={animal} animal={animal} /> : null;
+      })}
+    </ul>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const animals = ["Lion", "Cow", "Snake", "Lizard"];
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Animals: </h1>
+        <ul>
+          <List animals={animals} />
+        </ul>
       </div>
-      <h1>Hello World!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is FUCK YOUR CLICKS LOL{count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
