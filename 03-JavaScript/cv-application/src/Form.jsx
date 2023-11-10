c3eb94542deeff2eb798fc9a4e0fce5876d04848
Galaxeo <hwjustincheok@gmail.com>
2023-11-10 17:57:55 -0500

    feat: start cv-app

---
+++ 03-JavaScript/cv-application/src/Form.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Form() {
+  return (
+    <>
+      <input type="text" />
+    </>
+  );
+}
+
+export default Form;
